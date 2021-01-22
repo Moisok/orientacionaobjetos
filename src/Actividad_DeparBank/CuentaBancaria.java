e5e27b82c30aba3c1715218cfff61bc44878fbc8
Moises <Moisessepseg@gmail.com>
2021-01-22 13:55:45 +0100

    comienzo
    COmienzo de la actividad

---
+++ src/Actividad_DeparBank/CuentaBancaria.java
@@ -0,0 +1,26 @@
+package Actividad_DeparBank;
+
+public class CuentaBancaria {
+	
+	private String ivan;
+	
+	private String titular;
+	
+	private int saldo;
+	
+	private int ingreso;
+	
+	private int retirada;
+	
+	private int movimientos;
+	
+	
+	public CuentaBancaria (String ivan, String titular, int saldo) {
+		this.ivan=ivan;
+		this.titular=titular;
+		this.saldo=saldo;
+	}
+	
+	
+
+}
---
+++ src/Actividad_DeparBank/Deparbank.java
@@ -0,0 +1,27 @@
+package Actividad_DeparBank;
+
+public class Deparbank {
+
+	public static void main (String [] args){
+		
+		/*Creacion de variables*/
+		
+		/*Creacion de objeto*/
+		
+		/*Datos de la cuenta*/
+		
+		/*Nombre del titular*/
+		
+		/*Saldo disponible*/
+		
+		/*Ingresos*/
+		
+		/*Retirada de dineros*/
+		
+		/*Movimientos*/
+		
+		/*Salir del programa*/
+		
+	}
+	
+}
