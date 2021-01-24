6a9398bfd9172dd76ce51088a5e203d1180d826a
Moisok <77279331+Moisok@users.noreply.github.com>
2021-01-24 13:03:54 +0100

    Ejercicio del cajero realizado
    Ejercicio del cajero hecho, aunque hay alguna que otra cosa que pulir.

--- src/Actividad_DeparBank/CuentaBancaria.java
+++ src/Actividad_DeparBank/CuentaBancaria.java
@@ -1,12 +1,20 @@
 package Actividad_DeparBank;
 
+import java.util.Scanner;
+
 public class CuentaBancaria {
 	
-	private String ivan;
+	int opcion;
 	
-	private String titular;
+	String continuar;
 	
-	private int saldo;
+	static Scanner teclado = new Scanner (System.in);
+	
+	private static String iban;
+	
+	private static String titular;
+	
+	private float saldo;
 	
 	private int ingreso;
 	
@@ -14,13 +22,164 @@ public class CuentaBancaria {
 	
 	private int movimientos;
 	
+	private float contM;
 	
-	public CuentaBancaria (String ivan, String titular, int saldo) {
-		this.ivan=ivan;
+	private float contR;
+	
+	
+	public CuentaBancaria (String iban, String titular, float saldo) {
+		
+		this.iban=iban;
+		
 		this.titular=titular;
+		
 		this.saldo=saldo;
 	}
 	
+	/*Menu del cajero*/
+	
+	public void menu () {
+		
+		while (opcion < 7) {
+		System.out.println (" ");
+		System.out.println ("Elige opcion");
+		System.out.println ("Introduce 1 <- Para consultar los datos");
+		System.out.println ("Introduce 2 <- Mostrar titular");
+		System.out.println ("Introduce 3 <- Mostrar saldo");
+		System.out.println ("Introduce 4 <- Realizar ingreso");
+		System.out.println ("Introduce 5 <- Realizar retirada");
+		System.out.println ("Introduce 6 <- Consultar Movimientos");
+		System.out.println ("Introduce 7 <- Retirar tarjeta y salir");
+		System.out.print ("SELECCION: ");
+		
+		opcion = teclado.nextInt();
+		
+		if (opcion > 7) {
+			opcion = 0;
+			System.out.println (" ");
+			System.err.println ("La opcion no es valida");
+		}
+	
+		switch (opcion) {
+		
+		case 1:
+			opcion = 0;
+			System.out.println (" ");
+			System.out.println ("El titular de la cuenta es: " + titular);
+			System.out.println ("Su Nº de iban es: " + iban);
+			System.out.println ("Su saldo disponible es de " + saldo + "€");
+			System.out.println (" ");
+			System.out.println ("Introduzca 'c' para continuar");
+			System.out.print ("SELECCION: ");
+			continuar = teclado.next();
+			break;
+		case 2:	
+			opcion = 0;
+			System.out.println (" ");
+			System.out.println ("El titular de la tarjeta es: " + titular );
+			System.out.println (" ");
+			System.out.println ("Introduzca 'c' para continuar");
+			System.out.print ("SELECCION: ");
+			continuar = teclado.next();
+			break;
+		case 3:	
+			opcion = 0;
+			System.out.println (" ");
+			System.out.println ("Su saldo disponible es de " + saldo + "€");
+			System.out.println (" ");
+			System.out.println ("Introduzca 'c' para continuar");
+			System.out.print ("SELECCION: ");
+			continuar = teclado.next();
+			break;
+		case 4:	
+			opcion = 0;
+			System.out.println (" ");
+			ingresos(this.saldo);
+			System.out.println ("Tienes un saldo actual de: " + saldo + "€");
+			System.out.println (" ");
+			System.out.println ("Introduzca 'c' para continuar");
+			System.out.print ("SELECCION: ");
+			continuar = teclado.next();
+			break;
+		case 5:	
+			opcion = 0;
+			System.out.println (" ");
+			retirada(this.saldo);
+			System.out.println ("Tienes un saldo actual de: " + saldo + "€");
+			System.out.println (" ");
+			System.out.println ("Introduzca 'c' para continuar");
+			System.out.print ("SELECCION: ");
+			continuar = teclado.next();
+			break;
+		case 6:	
+			opcion = 0;
+			System.out.println (" ");
+			System.out.println ("Has ingresado dinero: " + ingreso + " veces");
+			System.out.println ("Has retirado dinero: " + retirada + " veces");
+			System.out.println (" ");
+			System.out.println ("Cantidad de dinero ingresada: " + contM + "€");
+			System.out.println ("Cantidad de dinero retirada: " + contR + "€");
+			System.out.println (" ");
+			System.out.println ("Actualmente tiene " + saldo + "€");
+			System.out.println (" ");
+			System.out.println ("Introduzca 'c' para continuar");
+			System.out.print ("SELECCION: ");
+			continuar = teclado.next();
+			break;
+		case 7:	
+			System.out.println (" ");
+			System.out.println ("Que tenga usted un buen dia y gracias por nuestros servicios");
+			break;
+	  }	
+	}
+  }
+	
+	/*Funcion para los ingresos*/
+	
+	public float ingresos (float saldo) {
+		this.saldo=saldo;
+		float ingreso = 0;
+		System.out.println (" ");
+		System.out.println ("Actualmente tiene " + saldo + "€");
+		System.out.println ("Introduce cantidad a ingresar: " );
+		System.out.print ("INGRESE(€): ");
+		ingreso = teclado.nextFloat();
+		this.saldo = this.saldo + ingreso;
+		if (ingreso >= 3000) {
+		System.err.println ("AVISO: NOTIFICACION A HACIENDA");
+		}
+		this.ingreso++;
+		this.contM= this.contM + ingreso;
+		return saldo;
+	}
+	
+	/*Funcion para retirar dinero*/
+	public float retirada (float saldo) {
+		this.saldo=saldo;
+		float retirada = 0;
+		System.out.println (" ");
+		System.out.println ("Actualmente tiene " + saldo  + "€");
+		System.out.println ("Introduce cantidad a retirar: " );
+		System.out.print ("RETIRE(€): ");
+		retirada = teclado.nextFloat();
+		this.saldo = this.saldo - retirada;
+		if (this.saldo < -50) {
+			System.out.println ("No hay suficiente saldo");
+			this.saldo = this.saldo + retirada;
+		}
+		else {
+		if (retirada >= 3000) {
+		System.err.println ("AVISO: NOTIFICACION A HACIENDA");
+		}
+		}
+		this.retirada++;
+		this.contR= this.contR + retirada;
+		return saldo;
+	}
+		
+}
+	
+	
+	
 	
 
-}
--- src/Actividad_DeparBank/Deparbank.java
+++ src/Actividad_DeparBank/Deparbank.java
@@ -1,26 +1,63 @@
 package Actividad_DeparBank;
 
+import java.util.Scanner;
+
 public class Deparbank {
 
 	public static void main (String [] args){
 		
-		/*Creacion de variables*/
 		
-		/*Creacion de objeto*/
+		Scanner teclado = new Scanner (System.in);
 		
-		/*Datos de la cuenta*/
+		int opcion;
+		String nombre;
+		int saldo;
+		String iban;
 		
-		/*Nombre del titular*/
+		System.out.println ("Bienvenido al cajero de DeparBank");
+		System.out.println ("A continuacion tendra que elegir");
+		System.out.println (" ");
+		System.out.println ("Opcion1: Ingresar tarjeta existente");
+		System.out.println ("Opcion2: Crear nueva cuenta bancaria");
+		System.out.println ("Opcion3: Salir");
+		System.out.print ("SELECCION: ");
+		opcion = teclado.nextInt();
 		
-		/*Saldo disponible*/
+		if (opcion > 3) {
+			System.err.println ("Opcion no Valida");
+		}
 		
-		/*Ingresos*/
+		switch (opcion) {
 		
-		/*Retirada de dineros*/
+		case 1:
+			
+			CuentaBancaria cuenta1 = new CuentaBancaria ("ES21 6500 5849 55 5659897474","Manolo",0);
+			cuenta1.menu();
+			break;
+			
+		case 2:
+			
+			System.out.println ("Introduce Nombre de titular: ");
+			System.out.print ("INTRODUCE NOMBRE: ");
+			nombre = teclado.next();
+			System.out.println ("¿Cuanto saldo quieres introducir?");
+			System.out.print ("INGRESE(€): ");
+			saldo = teclado.nextInt();
+			if (saldo >= 3000) {
+				System.err.println ("AVISO: NOTIFICACION A HACIENDA");
+				}
+			
+			CuentaBancaria cuenta2 = new CuentaBancaria ("ES21 6500 5849 55 5659897474",nombre,saldo);
+			cuenta2.menu();
+			break;
+			
+			
+		case 3:
+			System.out.println ("Que tenga usted un buen dia y gracias por nuestros servicios");
+			break;
+		}
 		
-		/*Movimientos*/
 		
-		/*Salir del programa*/
 		
 	}
 	
