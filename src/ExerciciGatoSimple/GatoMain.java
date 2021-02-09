8ae9312a854df1322566775f63c8b01e67d57e9c
Moises <Moisessepseg@gmail.com>
2021-02-09 11:42:01 +0100

    Gato terminado
    Ejercicio del gato terminado

--- src/ExerciciGatoSimple/GatoMain.java
+++ src/ExerciciGatoSimple/GatoMain.java
@@ -7,41 +7,18 @@ public class GatoMain {
 	public static void main(String[] args) {
 		/*Construccion de teclado*/
 		Scanner teclado = new Scanner(System.in);
-		
-		/*Variables del gato*/
-		String nombregato;
-		String colorgato;
-		String razagato;
-		int edad;
-		String sexogato;
-		float pesogato;
-		
-		/*Variables para clase y metodos*/
-		
+		/*Variables de comida*/
 		String comidadegato;
-		
-		/*Variables de menu*/
-		int opcion;
-		int opcion2;
 		
 		/*Comienza el programa*/
 		
 		/*Construimos dos gatos*/
 		
-		GatoSimple gato1 = new GatoSimple ("Misi","azul","siames",8,"hembra",5);
-
+		GatoSimple gato1 = new GatoSimple ("Misi","azul","siames",8,"macho",5);
 		GatoSimple gato2 = new GatoSimple ("Pepe","blanco","pardo",6,"macho",3);
-		
 		GatoSimple gato3 = new GatoSimple ("Crispin","gris","pardo",5,"macho",2);
-		
 		GatoSimple gato4 = new GatoSimple ("Batoni","rayasgrises","siames",9,"macho",3);
-		
 		GatoSimple gato5 = new GatoSimple ("Ramona","negro","siames",4,"hembra",5);
-		
-		/*Informacion de los gatos*/
-		System.out.println(gato1.toString());
-		System.out.println(gato2.toString());
-		
 		
 		/*Le damos de comer al gato, solo come si es pescado*/
 		System.out.println ("Dale de comer al gato");
@@ -58,10 +35,7 @@ public class GatoMain {
 		/*El gato pelea solo si ambos gatos son machos*/
 		gato1.pelea(gato2);
 		
-		
 		/*Contruccion del array de gatos*/
-		/*Hay que mostrar informacion for.echar, buscar en internet como funciona*/
-		/*Meter los gatos en el vector*/
 		GatoSimple gatetes [] = new GatoSimple [5];
 		gatetes [0] = gato1;
 		gatetes [1] = gato2;
@@ -69,9 +43,11 @@ public class GatoMain {
 		gatetes [3] = gato4;
 		gatetes [4] = gato5;
 						
-		
 		/*Bucle for each*/
 		
+		for (GatoSimple gatos : gatetes) {
+			System.out.println(gatos.toString());
+		}
 		
 	}
 
