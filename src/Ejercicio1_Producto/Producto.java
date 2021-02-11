496ab31f317b91f5e3d61457001127c1e25b0e45
Moisok <77279331+Moisok@users.noreply.github.com>
2021-02-11 18:44:45 +0100

    Ejercicio de producto terminado
    Ejercicio de producto terminado para mañana

---
+++ src/Ejercicio1_Producto/Producto.java
@@ -0,0 +1,32 @@
+package Ejercicio1_Producto;
+
+public class Producto {
+
+	String nombre;
+	int cantidad;
+	
+	
+	//Constructor
+	public Producto (String nombre, int Cantidad) {
+		this.nombre=nombre;
+		this.cantidad=Cantidad;
+	}
+
+	//Getters y setters
+	public String getNombre() {
+		return nombre;
+	}
+	
+	public int getCantidad() {
+		return cantidad;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	public void setCantidad(int cantidad) {
+		this.cantidad = cantidad;
+	}
+	
+}
